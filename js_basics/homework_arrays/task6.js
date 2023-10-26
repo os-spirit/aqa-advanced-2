e9494afbfaf58c0dc44aec5e61f458de184e7bbf
Valentin Nalivayko <valentinnalivayko@Valentins-MacBook-Pro.local>
2023-10-26 18:55:50 +0300

    homework_arrays

---
+++ js_basics/homework_arrays/task6.js
@@ -0,0 +1,18 @@
+// Завдання 6: Сортування масивів
+
+// У вас є вихідний масив
+
+// сonst numbersList = [1,10,14,2,4,5,43,34]
+// Створіть новий масив на основі вихідного масиву (копіюйте його)
+// Відсортуйте створений масив (від меншого до більшого)
+// Виведіть обидва масиви в консоль
+
+const numbersList = [1,10,14,2,4,5,43,34];
+
+const numbersListSortedCopy = numbersList.map((elements)=> elements);
+
+numbersListSortedCopy.sort((a, b) => a - b);
+
+console.log(numbersList);
+
+console.log(numbersListSortedCopy);
